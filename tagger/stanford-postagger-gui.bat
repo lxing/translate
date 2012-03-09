75ddcbb245b14c091b1a56d3210e0fc3221f5355
Alexander Chia <alexchia@stanford.edu>
2012-03-09 00:23:08 -0800

    misc edits

--- tagger/stanford-postagger-gui.bat
+++ tagger/stanford-postagger-gui.bat
@@ -1,3 +1,3 @@
 :: runs the POS tagger (toy) GUI
 :: usage stanford-postagger-gui
-java -mx200m -cp "stanford-postagger.jar;" edu.stanford.nlp.tagger.maxent.MaxentTaggerGUI
+java -mx1g -cp "stanford-postagger.jar;" edu.stanford.nlp.tagger.maxent.MaxentTaggerGUI
